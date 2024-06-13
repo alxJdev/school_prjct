e4d6e797430559a3da120b66f3094ebbe7c19a03
alx <alexander.voss@alx-dev.net>
2024-06-13 08:22:50 +0200

    final changes for real this time

--- src/snd.js
+++ src/snd.js
@@ -2,7 +2,13 @@ import {Comm} from "./comm.js";
 import {Crypto} from "alxhub_crypto/src/crypto.js";
 import * as readline from "node:readline";
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 const comm = new Comm("/dev/ttyUSB1");
-//comm.Open();
-
-await comm.Send("Test Data #1");
+rl.question("Enter Message:", async (msg) => {
+    await comm.Send(msg);
+    console.log(`Message Send: ${msg}`)
+    rl.close();
+});
